380c33ae1daaf359a1775f751982223f5484ad35
SergEnergy <kis8883244@gmail.com>
2023-12-06 06:38:08 +0300

    feat: work with custom user property and full tailwind css - can change property

--- try-tailwindcss3/tailwind.config.js
+++ try-tailwindcss3/tailwind.config.js
@@ -3,7 +3,21 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        layout: 'auto 1fr auto',
+      },
+      colors: {
+        'logo-main': '#9c0303',
+        'logo-purple': {
+          100: '#bbcafe',
+          200: '#fff232',
+        },
+      },
+      fontFamily: {
+        main: ['Montserrat'],
+      },
+    },
   },
   plugins: [],
 };
