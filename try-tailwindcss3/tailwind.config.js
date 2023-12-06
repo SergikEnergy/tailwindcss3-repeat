e3fbd8700398d147bb34039cc48698ac634a32a7
SergEnergy <kis8883244@gmail.com>
2023-12-06 06:01:25 +0300

    feat: work with dark mode without js

--- try-tailwindcss3/tailwind.config.js
+++ try-tailwindcss3/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
+// darkMode - only for dev - for prod is it unnecessary - because theme wiil be changed after changing theme inside windows or browser
